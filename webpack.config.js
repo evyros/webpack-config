5694375e9fd7a66bc1a3f1de2268faaaf5824dc6
Evyatar Rosner <evyatar.rosner@tikalk.com>
2020-12-09 15:07:59 +0200

    html-webpack-plugin integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/main.js',
@@ -6,6 +7,9 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'scripts.bundle.js'
 	},
+	plugins: [
+		new HtmlWebpackPlugin({template: './src/index.html'}),
+	],
 	mode: 'production',
 	devtool: 'source-map'
 };
